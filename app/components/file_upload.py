0a44b345020633d06a3077bbad404cd2ec8198a7
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2024-09-19 15:49:41 +0100

    added spinner while api being called

--- app/components/file_upload.py
+++ app/components/file_upload.py
@@ -4,22 +4,35 @@ from utils import parse_contents, check_file
 from dash.exceptions import PreventUpdate
 
 title_and_tooltip = html.Span([
+    # dbc.Row([
+    #     dbc.Col(
+    #         html.H2(
+    #             "Share Audio Transcriber", id = "page-title", className="text-center mb-4"
+    #         ),
+    #         className="d-flex justify-content-center"
+    #     ),
+    #     dbc.Col([
+    #         dbc.Label(className="fa fa-circle-info", id = "title-tooltip", html_for="page-title"),
+    #         dbc.Tooltip("Currently only accepting m4a files (can export videos as 'audio only'), contact Data Science team to add compatible formats",
+    #                     id="tooltip",
+    #                     is_open=False,
+    #                     target="title-tooltip",
+    #                     placement = "right")
+    #     ], className="d-flex justify-content-center", width={"size": 6, "offset": 3})
+    # ])
     dbc.Row([
-        dbc.Col(
-            html.H2(
-                "Share Audio Transcriber", id = "page-title", className="text-center mb-4"
-            ),
-            className="d-flex justify-content-center"
-        ),
-        dbc.Col([
-            dbc.Label(className="fa fa-circle-info", id = "title-tooltip", html_for="page-title"),
-            dbc.Tooltip("Currently only accepting m4a files (can export videos as 'audio only'), contact Data Science team to add compatible formats",
-                        id="tooltip",
-                        is_open=False,
-                        target="title-tooltip",
-                        placement = "right")
-        ], className="d-flex justify-content-center", width={"size": 6, "offset": 3})
-    ])
+    dbc.Col([
+        html.H2("Share Audio Transcriber", id="page-title", className="text-center mb-4 d-inline-block", style={"white-space": "nowrap"}),
+        dbc.Label(className="fa fa-circle-info ms-2 d-inline-block", id="title-tooltip", html_for="page-title", style={"position": "relative", "top": "-1mm", 'color': '#7800c6'}),
+        dbc.Tooltip(
+            "Currently only accepting m4a files (can export videos as 'audio only'), contact Data Science team to add compatible formats",
+            id="tooltip",
+            is_open=False,
+            target="title-tooltip",
+            placement="right"
+        )
+    ], className="d-flex justify-content-center align-items-center", width={"size": 6, "offset": 3})
+])
 ])
 
 file_upload_widget = html.Span([
@@ -30,6 +43,8 @@ file_upload_widget = html.Span([
     ),
     dbc.Spinner(html.Div(id = "loading-output")),
     html.Div(id = "upload-status"),
+    dbc.Spinner(dcc.Download(id="download-transcript"), 
+                spinner_style= {'border-color': '#bb7fe2', 'border-right-color': 'transparent'}),
     dbc.Progress(value=0, style={"margin-top": "20px", "margin-bottom": "20px", "height": "20px"}),
     dbc.RadioItems(
         id = "action-input", value="transcriptions", inline = True, className = "2mt-3",
@@ -51,8 +66,8 @@ file_upload_widget = html.Span([
         )
     ]),
     dbc.Button("Process Video", id = "go-button", color="primary", className="mt-3", n_clicks = 0),
+    # dbc.Spinner(html.Div(id = "process-file-spinner", color = "primary")),
     # dbc.Button("Download Transcript", id = "download-button", color="secondary", className="mt-3", n_clicks = 0),
-    dcc.Download(id="download-transcript") # just download automatically?
 ])
 
 
@@ -77,7 +92,7 @@ def update_output(n_clicks, action, response_format, content, name):
         children = [
             parse_contents(action, content, response_format)
         ]
-        return 'Download your transcript', dict(content=children, filename="".join({"transcript.", response_format}))
+        return None, dict(content=children, filename="".join({"transcript.", response_format}))
 
 @callback(
     Output("loading-output", "children"),
@@ -91,3 +106,15 @@ def show_upload_progress(contents, filename):
         raise PreventUpdate
     
     return None, f"Uploaded file: {filename}"
+
+# @callback(
+#     Output("process-file-spinner", "children"),
+#     Input("go-button", "n_clicks"),
+#     State("upload-data", "filename"),
+#     prevent_initial_call=True
+# )
+# def show_upload_progress(contents, filename):
+#     if contents is None:
+#         raise PreventUpdate
+    
+#     return None, f"Uploaded file: {filename}"
